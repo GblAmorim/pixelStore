317fbefe0a9942edb90bd7a582f1dad3a26afeb0
GblAmorim <gabrielmorim98@gmail.com>
2018-06-26 21:59:26 -0300

    comçando o projeto

---
+++ view/home.php
@@ -0,0 +1,17 @@
+<head>
+	<title>Pixel Store</title>
+
+	<!-- Estilos da página -->
+	
+    <!-- <link rel="stylesheet" href="css/carousel.css">
+	<link rel="stylesheet" href="css/footer.css" > -->
+</head>
+
+	<?php
+		include('header.php');
+	?>
+
+
+<?php
+	//include('footer.php');
+?>
